ea0e2bbbb5d92595081b40a616f922c6c49ffa6d
Benjamin Heineike <heineib@ca002.camp.thecrick.org>
2022-02-01 16:09:17 +0000

    addid gal_dalal_sc and gluc_rep as visualization options in two_spec_compare

--- 20220128_two_spec_compare.R
+++ 20220128_two_spec_compare.R
@@ -173,11 +173,12 @@ output_comb = spec_compare(output_cond = output_cond, specA = specA, specB= spec
 
 
 
-vis_option = 'sc_gal'
+vis_option = 'gal_dalal_sc'
 
 #Possible vis options: 
 #orth_type:  Ortholog type 
 #sc_gal: 4 top S. cerevisiae gal genes (defined below)
+#gluc_rep: glucose repression genes from Kayikci_Nielsen et al ????
 #pka_inh and pka_rep:  Pka repression genes from Heineike et al 2020. 
 
 #  Gal Pathway 
@@ -189,13 +190,26 @@ vis_option = 'sc_gal'
 
 sc_gal_list = c('P04385', 'P04397', 'P08431', 'P13181')
 
+
+#Gal genes from Dalal et al 2016
+
+if (vis_option %in% c('gal_dalal_sc','gal_dalal_ca')) {
+  gal_dalal_sc = read.csv(file=paste(base_dir, 'diverse_strains/processed_data/external_data/gal_dalal/sc_gal.csv', sep=''))
+  output_comb$gal_dalal_sc= FALSE
+  output_comb$gal_dalal_sc[which(output_comb$sc_orf %in% intersect(output_comb$sc_orf, gal_dalal_sc$Systematic.Name))] = TRUE
+}
+
+
+
+
+
 output_comb$sc_gal = FALSE
 output_comb$sc_gal[which(output_comb$genename_B %in% intersect(output_comb$genename_B, sc_gal_list))] = TRUE
 
 
 #  PKA genes
 if (vis_option %in% c('pka_act', 'pka_rep')){
-  sc_pka_inh = read.csv(file=paste(base_dir, 'external_data/pka_inhibition/20181017_deseq_SC_AS_WT_nmpp1.csv',sep=''), header=TRUE, row.names=1 )  
+  sc_pka_inh = read.csv(file=paste(base_dir, 'diverse_strains/processed_data/external_data/pka_inhibition/20181017_deseq_SC_AS_WT_nmpp1.csv',sep=''), header=TRUE, row.names=1 )  
   
   #PKA activation and inhibition as defined in Heineike et al 2021
   pka_act = sc_pka_inh[which(sc_pka_inh$log2FoldChange>2.0 & sc_pka_inh$padj< 0.001),]
@@ -207,10 +221,19 @@ if (vis_option %in% c('pka_act', 'pka_rep')){
   }
 
 
+# Gluc Repression
+if (vis_option == 'gluc_rep') {
+  gluc_rep = read.csv(file=paste(base_dir, 'diverse_strains/processed_data/external_data/gluc_rep/Proteins_C_metabolite_repression_Kayikci_Nielsen_final.csv', sep=''))
+  output_comb$gluc_rep= FALSE
+  output_comb$gluc_rep[which(output_comb$genename_B %in% intersect(output_comb$genename_B, gluc_rep$uniprot_id))] = TRUE
+  }
+
+
+
+
 # Other visualization options:  
 #  Ohnologs
 #  Cytoplasmic translation
-
 
 
 #plot scatter plots using Plotly 
