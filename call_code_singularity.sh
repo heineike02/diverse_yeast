721a255cc55a8085824d8c5adc098e50c3fa3d4c
benjamin.heineike@charite.de <heineikeb@s-mcpb-ms03.charite.de>
2024-07-31 16:14:29 +0000

    ran Branch Site for OG1122

--- call_code_singularity.sh
+++ call_code_singularity.sh
@@ -12,11 +12,11 @@ cd ~/singularity
 #singularity exec codeml.sif /home/heineikeb/github/diverse_yeast/20221206_struct_align_dnds_msas.sh
 #singularity exec codeml.sif /home/heineikeb/github/diverse_yeast/20221214_run_m0.sh
 #singularity exec codeml.sif /home/heineikeb/github/diverse_yeast/20230119_run_m1.sh
-#singularity exec codeml.sif /home/heineikeb/github/diverse_yeast/20221214_run_BS.sh
+singularity exec codeml.sif /home/heineikeb/github/diverse_yeast/20221214_run_BS.sh
 #singularity exec codeml.sif /home/heineikeb/github/diverse_yeast/20230309_run_m0_troubleshoot.sh
 #singularity exec codeml.sif /home/heineikeb/github/diverse_yeast/20230110_seq_align.sh
 #singularity exec codeml.sif /home/heineikeb/github/diverse_yeast/20230324_seq_align_dnds_trees.sh
 #singularity exec codeml.sif /home/heineikeb/github/diverse_yeast/20230326_calculate_tree_quantities.sh
 #singularity exec codeml.sif /home/heineike_wsl2/github_s/diverse_yeast/20221207_run_yn00.sh
 #singularity exec codeml.sif /home/heineikeb/github/diverse_yeast/20240109_struct_align_dnds_msas_uniprot_binding_site.sh
-singularity exec codeml.sif /home/heineikeb/github/diverse_yeast/20240109_run_m0_features.sh
+#singularity exec codeml.sif /home/heineikeb/github/diverse_yeast/20240109_run_m0_features.sh
