dc37c5aa5f9f8774f72a062a2bb30f2efffa1e3d
Ben Heineike <heineike02@gmail.com>
2025-02-11 13:52:55 +0000

    Evolutionary Coupling of for cox9/cox2 and analysis of TDH1/2/3

--- 20250122_evcoupling.py
+++ 20250122_evcoupling.py
@@ -4,7 +4,8 @@ from evcouplings.utils.pipeline import execute
 
 base_dir = os.path.normpath('/home/heineike/alphafold') #Ben's computer
 
-print(base_dir)
-config2 = read_config_file(base_dir + os.sep + os.path.normpath('examples/etc/coupling_analysis/ec_og1254_config.txt'))
-print(config2)
-outcfg = execute(**config2)
+#config = read_config_file(base_dir + os.sep + os.path.normpath('examples/etc/coupling_analysis/COX9_COX2/ec_config_COX9.txt'))
+#config = read_config_file(base_dir + os.sep + os.path.normpath('examples/etc/coupling_analysis/COX9_COX2/ec_config_COX2.txt'))
+config = read_config_file(base_dir + os.sep + os.path.normpath('examples/etc/coupling_analysis/COX9_COX2/ec_config_COX9_COX2_concat.txt'))
+print(config)
+outcfg = execute(**config)
