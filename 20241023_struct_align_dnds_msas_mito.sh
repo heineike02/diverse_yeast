1c3c7e258dbed25f5724c9e6dea516ed0f1bb44c
Ben Heineike <heineike02@gmail.com>
2025-05-01 17:44:29 +0100

    Updated smaller files and readme and moved various files to archive

--- 20241023_struct_align_dnds_msas_mito.sh
+++ 20241023_struct_align_dnds_msas_mito.sh
@@ -3,4 +3,4 @@
 . /opt/conda/etc/profile.d/conda.sh
 conda activate diverse_yeast_env
 
-python ~/github/diverse_yeast/20241023_struct_align_dnds_trees_mito.py
+python ~/github/diverse_yeast/20241023_struct_align_dnds_msas_mito.py
--- bash_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-python3 20241025_site_model_check_run.py
-
-
-while read p; do
-  echo "$p"
-done < /home/heineike/alphafold_ln/selection_calculations/site_model/uncalculated_ogs.txt
